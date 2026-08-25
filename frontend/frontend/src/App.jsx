20ef38fc8f6b718d1c2c82df53d3e2db84b04c26
user.email like:"suparsh001@gmail.com <user.email like:"suparsh001@gmail.com>
2026-08-25 16:40:24 +0530

    Front end integrated

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+    <Route path="/" element={<h1> ParkCar</h1>}/>
+
+</Routes>
+
+
+</BrowserRouter>
+  )
+}
+
+export default App
